720454473b31f46842cfa4e520dd04c4d8b1a8ea
LazyChun <1299362885@qq.com>
2021-02-25 23:25:49 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
-
+import React ,{useState}from "react";
+import { SketchPicker } from 'react-color'
 const appStyle = {
   maxWidth: 1300,
-  minHeight: 574
+  height: 574,
+  display:"flex",
+  background:"white",
+  justifyContent:"center",
+  alignItems:"center"
 };
 
 const App = () => {
-  return <div style={appStyle}>Welcome to LazyChun !</div>;
+  const [color,setColor] = useState("#333");
+  return <div style={appStyle}>
+    <SketchPicker width={500} color={color} onChangeComplete={ ({ hex })=>setColor(hex) } />
+  </div>;
 };
 
 export default App;
